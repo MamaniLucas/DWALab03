2bd17089f0b189e013ee17e96efada3d7d25e191
Kenziefor <myriam.mamani@tecsup.edu.pe>
2023-09-12 23:49:07 -0500

    primer commit

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 
+const { config } = require('dotenv');
+const pg = require('pg');
+
 const express = require('express')
+config()
 
 const app = express()
 
 const port = 4500
 
-
-app.get("/", (req, res) => {
-    res.send('HELLO WORLD RENDER !')
-    root = __dirname
+const pool =  new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
 })
+app.get("/", (req, res) => {
+    res.send('HELLO WORLD RENDER !') 
+})
+
+app.get("/ping", async (req, res) => { 
+    const result = await pool.query('SELECT NOW()')
+    return res.json(result.rows[0])
+})
+
+
 app.listen(port)
 console.log(`Server on port, ${port}`)
